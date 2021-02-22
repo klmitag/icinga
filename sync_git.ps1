baaaf3b656bcf51b5d6d58f358efeac1a129da88
klmitag <68317634+klmitag@users.noreply.github.com>
2021-02-22 17:37:57 +0100

    Delete sync_git.ps1

--- sync_git.ps1
+++
@@ -1,3 +0,0 @@
-﻿$GitHubRepository = 'icinga'$GitHubLocalRepoRoot = $env:USERPROFILE + '\Git'$GitHubRepositoryRoot = $GitHubLocalRepoRoot + '\' + $GitHubRepository
-Set-Location $GitHubRepositoryRoot
-$sync = git pull upstream master

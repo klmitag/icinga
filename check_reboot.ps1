6955decc5d0f41698e63fee1686cf9165894120d
klmitag <68317634+klmitag@users.noreply.github.com>
2021-02-22 15:45:55 +0100

    Add files via upload

---
+++ check_reboot.ps1
@@ -0,0 +1,21 @@
+﻿#$ReturnCode:
+# 0 > UP, OK
+# 1 > UP, Warning
+# 2 > DOWN, Critical
+# 3 > DOWN, Unknown
+
+$value = test-path -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
+	 
+# If path does not exist, return OK status
+if ($value -match "False") {
+    echo "OK - No reboot required"
+    $returnCode=0
+}
+	 
+# Else return WARNING status
+else {
+    echo "WARNING - Reboot required"
+    $returnCode=1
+}
+
+Exit ($ReturnCode)
